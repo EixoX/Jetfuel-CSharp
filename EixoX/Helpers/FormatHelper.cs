9ad2286c6662470429bea41cd0c96e02ca600e8d
Luiz Henrique Strobelt Pérez <lhsperez@gmail.com>
2013-03-12 17:20:11 -0300

    FormatHelper and CreditCardNumber

--- EixoX/Helpers/FormatHelper.cs
+++ EixoX/Helpers/FormatHelper.cs
@@ -13,5 +13,18 @@ namespace EixoX
         {
             return string.Concat((value / 1000).ToString("00000"), "-", (value % 1000).ToString("000"));
         }
+
+
+        public static string DigitsOnly(string value)
+        {
+            if(string.IsNullOrEmpty(value))
+                return value;
+
+            StringBuilder builder = new StringBuilder(value.Length);
+            for (int i = 0; i < value.Length; i++)
+                if (Char.IsDigit(value, i))
+                    builder.Append(value[i]);
+            return builder.ToString();
+        }
     }
 }
---
+++ EixoX/Restrictions/CreditCardNumber.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace EixoX.Restrictions
+{
+    [Serializable]
+    [AttributeUsage(AttributeTargets.Field | AttributeTargets.Property, AllowMultiple = false, Inherited = true)]
+    public class CreditCardNumber :
+        Attribute,
+        Restriction
+    {
+        public bool Validate(object input)
+        {
+            if (input == null)
+                return false;
+
+            string digits = FormatHelper.DigitsOnly(input.ToString());
+
+            if (digits.Length < 12 | digits.Length > 19)
+                return false;
+
+            char[] digitsReversed = new char[digits.Length];
+            for (int i = 0; i < digitsReversed.Length; i++)
+                digitsReversed[i] = digits[digits.Length - i - 1];
+
+            int luhn = 0;
+            for (int i = 0; i < digitsReversed.Length; i++)
+            {
+                int d = i % 2 == 1 ? 2 * int.Parse(digitsReversed[i].ToString()) : int.Parse(digitsReversed[i].ToString());
+                luhn += (d / 10) + (d % 10);
+            }
+            return (luhn != 0) && (luhn % 10 == 0);
+        }
+
+
+        public string GetRestrictionMessage(object input)
+        {
+            return "Invalid credit card number";
+        }
+    }
+}
