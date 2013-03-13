1885fda949cca69b3f5fb37018c60e7e8fbd2203
Rodrigo Portela <rodrigo.portela@gmail.com>
2013-03-13 16:49:18 -0300

    cpf format

--- EixoX/Helpers/FormatHelper.cs
+++ EixoX/Helpers/FormatHelper.cs
@@ -6,7 +6,7 @@ namespace EixoX
 {
     public static class FormatHelper
     {
-        
+
 
 
         public static string Cep(int value)
@@ -14,10 +14,22 @@ namespace EixoX
             return string.Concat((value / 1000).ToString("00000"), "-", (value % 1000).ToString("000"));
         }
 
+        public static string Cpf(long value)
+        {
+            return
+                string.Concat(
+                    ((value / 100000000) % 1000).ToString("000"),
+                    ".",
+                    ((value / 100000) % 1000).ToString("000"),
+                    ".",
+                    ((value / 100) % 1000).ToString("000"),
+                    "-",
+                    (value % 100).ToString("00"));
+        }
 
         public static string DigitsOnly(string value)
         {
-            if(string.IsNullOrEmpty(value))
+            if (string.IsNullOrEmpty(value))
                 return value;
 
             StringBuilder builder = new StringBuilder(value.Length);
