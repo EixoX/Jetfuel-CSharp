a0136caf230e01fe183c0a09bc5291aadf1ad9a3
Rodrigo Portela <rodrigo.portela@gmail.com>
2013-04-18 15:50:04 -0300

    start of extensions

---
+++ EixoX.Extensions/XmlExtensions.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace System.Xml
+{
+    public static class XmlExtensions
+    {
+        public static IEnumerable<XmlElement> ChildElements(this XmlElement element)
+        {
+            foreach (XmlNode child in element.ChildNodes)
+                if (child is XmlElement)
+                    yield return child as XmlElement;
+        }
+
+    }
+}
---
+++ EixoX/LazyConstructor.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace EixoX
+{
+    public struct LazyConstructor<T>
+        where T : new()
+    {
+        private T _Value;
+
+
+        public T Value
+        {
+            get
+            {
+                if (_Value == null)
+                    _Value = new T();
+                return _Value;
+            }
+        }
+
+
+        public static implicit operator T(LazyConstructor<T> item)
+        {
+            return item.Value;
+        }
+
+
+
+    }
+}
