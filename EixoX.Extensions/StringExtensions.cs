d84ef4c5274a9bd472f3b29f7c7d11519fe3ef88
Rodrigo Portela <rodrigo.portela@gmail.com>
2013-04-26 18:03:53 -0300

    String extensions

---
+++ EixoX.Extensions/StringExtensions.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace System
+{
+    public static class StringExtensions
+    {
+
+        public static string Left(this string input, int length)
+        {
+            if (string.IsNullOrEmpty(input))
+                return input;
+            else if (length > input.Length)
+                return input;
+            else
+                return input.Substring(0, length);
+        }
+    }
+}
