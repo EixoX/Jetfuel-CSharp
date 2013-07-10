9fa7bbf5d47060a4b72a462bf360dd2e303f145e
Rodrigo Portela <rodrigo.portela@gmail.com>
2013-07-10 17:28:48 -0300

    Email template and string tokenizer

--- EixoX.Extensions/TextWriterExtensions.cs
+++ EixoX.Extensions/TextWriterExtensions.cs
@@ -1,8 +1,9 @@
 ﻿using System;
 using System.Collections.Generic;
-using System.IO;
 using System.Linq;
 using System.Text;
+using System.IO;
+using EixoX.Html;
 
 namespace System
 {
@@ -12,8 +13,31 @@ namespace System
         {
             writer.Write(name);
             writer.Write("=\"");
-            writer.Write(value == null ? "" : value.ToString());
+            writer.Write(value == null ? "": value.ToString().Replace("\"", "&quot;"));
             writer.Write("\"");
         }
+
+        public static void WriteHtmlTag(this TextWriter writer, string tagName, bool isEmpty, params HtmlAttribute[] attributes)
+        {
+            writer.Write('<');
+            writer.Write(tagName);
+            for (int i = 0; i < attributes.Length; i++)
+            {
+                writer.Write(' ');
+                writer.Write(attributes[i].Name);
+                writer.Write("=\"");
+                writer.Write(attributes[i].Value == null ? "" : attributes[i].Value.ToString().Replace("\"", "&quot;"));
+                writer.Write('"');
+            }
+            writer.Write(isEmpty ? " />" : ">");
+        }
+
+        public static void WriteHtmlCloseTag(this TextWriter writer, string tagName)
+        {
+            writer.Write("</");
+            writer.Write(tagName);
+            writer.Write(">");
+        }
+
     }
 }
