1911b20983c72edb872f2877a22680fbfb207f11
Rodrigo Portela <rodrigo.portela@gmail.com>
2013-08-15 14:32:50 -0300

    Added eixox.brasil

---
+++ EixoX.Brasil/Logradouro.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using EixoX.Data;
+namespace EixoX.Brasil
+{
+
+    [DatabaseTable]
+    public class Logradouro : BrasilDbModel<Logradouro>
+    {
+        [DatabaseColumn(DatabaseColumnKind.Unique)]
+        public int Cep { get; set; }
+        [DatabaseColumn]
+        public int CodigoCepLogradouro { get; set; }
+        [DatabaseColumn]
+        public int CodigoCepBairro { get; set; }
+        [DatabaseColumn]
+        public int CodigoCepTipo { get; set; }
+        [DatabaseColumn]
+        public string Nome { get; set; }
+
+    }
+}
---
+++ EixoX.Brasil/Microregiao.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using EixoX.Data;
+
+namespace EixoX.Brasil
+{
+    [DatabaseTable]
+    public class Microregiao : BrasilDbModel<Microregiao>
+    {
+        [DatabaseColumn(DatabaseColumnKind.Unique)]
+        public int CodigoIbge { get; set; }
+        [DatabaseColumn]
+        public int CodigoIbgeEstado { get; set; }
+        [DatabaseColumn]
+        public int CodigoIbgeMesoregiao { get; set; }
+        [DatabaseColumn]
+        public string Nome { get; set; }
+
+    }
+}
