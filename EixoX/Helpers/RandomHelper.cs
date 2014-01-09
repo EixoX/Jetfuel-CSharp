7b4e295f9f26c01c394519ad7057d9e996def2bb
Rodrigo Portela <rodrigo.portela@gmail.com>
2014-01-09 16:33:18 -0200

    Random Helper Methods

--- EixoX/Helpers/RandomHelper.cs
+++ EixoX/Helpers/RandomHelper.cs
@@ -24,5 +24,26 @@ namespace EixoX.Helpers
         {
             return theRandom.Next(minInclusive, maxExclusive);
         }
+
+        public static string RandomLetters(int size)
+        {
+            char[] arr = new char[size];
+            for (int i = 0; i < arr.Length; i++)
+            {
+                arr[i] = (char)theRandom.Next(97, 123);
+            }
+            return new string(arr);
+        }
+
+        public static string RandomLetterOrDigits(int size)
+        {
+            char[] arr = new char[size];
+            for (int i = 0; i < arr.Length; i++)
+            {
+                int r = theRandom.Next(97, 133);
+                arr[i] = r < 123 ? (char)r : (char)(r - 75);
+            }
+            return new string(arr);
+        }
     }
 }
