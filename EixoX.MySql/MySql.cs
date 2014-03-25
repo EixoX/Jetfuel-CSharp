1d26237484ee23a542d54984e3055b11160e46a9
Rodrigo Portela <rodrigo.portela@gmail.com>
2014-03-25 11:38:25 -0300

    merge mysql conflict

---
+++ EixoX.MySql/MySql.cs
@@ -0,0 +1,14 @@
+﻿using EixoX.Data;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EixoX.MySql
+{
+    public class MySqlDatabase : Database
+    {
+        public MySqlDatabase(string connectionString) : base(new MySqlDialect(), connectionString) { }
+    }
+}
--- EixoX.MySql/MySqlDialect.cs
+++ EixoX.MySql/MySqlDialect.cs
@@ -1,19 +1,24 @@
 ﻿using EixoX.Data;
+using MySql.Data.MySqlClient;
 using System;
 using System.Collections.Generic;
-using System.Linq;
 using System.Text;
-using System.Threading.Tasks;
 
 namespace EixoX.MySql
 {
     public class MySqlDialect : AnsiDialect
     {
-        public MySqlDialect() : base('`', '`') { }
+
+        public MySqlDialect() : base('`','`')
+        {
+
+        }
 
         public override System.Data.IDbConnection CreateConnection(string connectionString)
         {
-            return null;
+            return new MySqlConnection(connectionString);
         }
+
+
     }
 }
