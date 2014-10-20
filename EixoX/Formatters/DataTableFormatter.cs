70a9f1a509c483bd3e8e4e9d198ee1601bc5e6da
Rodrigo Portela <rodrigo.portela@gmail.com>
2014-10-20 18:40:24 -0200

    Csv formatter

---
+++ EixoX/Formatters/DataTableFormatter.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.IO;
+using System.Text;
+
+namespace EixoX.Formatters
+{
+    public class DataTableFormatter
+    {
+
+        public static void ToCsv(DataTable dataTable, string separator, TextWriter writer, IFormatProvider formatProvider)
+        {
+            int colCount = dataTable.Columns.Count;
+            writer.Write(dataTable.Columns[0].ColumnName);
+            for (int i = 1; i < colCount; i++)
+            {
+                writer.Write(separator);
+                writer.Write(dataTable.Columns[i].ColumnName);
+            }
+            writer.WriteLine();
+            foreach (DataRow row in dataTable.Rows)
+            {
+                writer.Write(string.Format(formatProvider, "{0}", row[0]));
+                for (int i = 1; i < colCount; i++)
+                {
+                    writer.Write(separator);
+                    writer.Write(string.Format(formatProvider, "{0}", row[i]));
+                }
+                writer.WriteLine();
+            }
+        }
+    }
+}
