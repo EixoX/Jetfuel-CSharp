d91eb23fcd09f3153a3915bdb5e5fac17ec6104a
Guilherme Rey <poli.rey@gmail.com>
2014-12-15 16:22:38 -0200

    QueryStringHelper

---
+++ EixoX.Web/QueryStringHelper.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EixoX.Web
+{
+    public class QueryStringHelper
+    {
+        public static string Build(params Tuple<string, string>[] tuples)
+        {
+            string[] parameters = new string[tuples.Length];
+            for (int i = 0; i < parameters.Length; i++)
+                parameters[i] = tuples[i].Item1 + "=" + tuples[i].Item2;
+
+            return String.Join("&", parameters);
+        }
+    }
+}
