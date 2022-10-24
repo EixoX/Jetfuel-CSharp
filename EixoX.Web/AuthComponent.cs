1cc1c5e5886569b6b2c719c0b6a8c2eee84cda4b
Carlos dos Santos <cds@cds-software.com.br>
2022-10-24 13:37:56 -0300

    AuthComponent

---
+++ EixoX.Web/AuthComponent.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace EixoX.Web.AuthComponent
+{
+    //public abstract class AuthenticationBasedController<T> : Controller
+    //{
+    //    public abstract T CurrentUser { get; set; }
+    //    public abstract string LoginUrl { get; }
+    //    public bool IsLoggedIn => CurrentUser != null;
+    //}
+
+    public class AuthenticationRequired : Attribute
+    {
+
+    }
+
+}
